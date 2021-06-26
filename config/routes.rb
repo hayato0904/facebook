9f76ec7152d727051537355379de21ef40f6ba96
hayato0904 <sakigi94@gmail.com>
2021-06-26 17:29:30 +0000

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root  'users#index' 
   resources :blogs
   resources :contacts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  
 
 end
